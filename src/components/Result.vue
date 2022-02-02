9ac055394a23832100ec5da59b02dc0494f34c55
Georg M. Sorst <g.sorst@findologic.com>
2022-02-02 18:52:37 +0100

    Refine product cards a bit

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,8 +1,17 @@
 <template>
-  <v-card class="fill-height">
+  <v-card class="fill-height d-flex flex-column">
     <v-img :src="result.imageUrl" contain height="250"></v-img>
     <v-card-title><a :href="result.url">{{ result.name }}</a></v-card-title>
     <v-card-text>€ {{ result.price }}</v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-heart-outline</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-cart</v-icon>
+      </v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
