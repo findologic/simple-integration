ec84bd8c7f968e0d5a942fbe4015748f8c09978a
Georg M. Sorst <g.sorst@findologic.com>
2022-02-04 12:15:53 +0100

    Use better dummy data

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,8 +1,17 @@
 <template>
-  <v-card class="fill-height">
-    <v-img contain height="250" src="findologic.png"></v-img>
-    <v-card-title><a href="https://example.com">Awesome Product</a></v-card-title>
-    <v-card-text>€ 8.15</v-card-text>
+  <v-card class="fill-height d-flex flex-column">
+    <v-img :src="`https://www.fillmurray.com/250/${Math.round(Math.random()*200+100)}`" contain height="250"></v-img>
+    <v-card-title><a href="https://example.com">Awesome Product {{ (Math.random() * 10).toFixed(0) }}</a></v-card-title>
+    <v-card-text>€ {{ (Math.random() * 10).toFixed(2) }}</v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-heart-outline</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-cart</v-icon>
+      </v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
